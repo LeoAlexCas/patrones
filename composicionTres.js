9ab8badf3ac1e29caaf9c6714946b3130d05754f
Leonardo <castroleonardo@live.cl>
2022-05-31 20:49:14 -0400

    composicion con pipe

--- composicionTres.js
+++ composicionTres.js
@@ -1,3 +1,5 @@
+//misma implementacion, pero con una funcion pipe, en realidad es lo mismo, pero con un reduce normal
+
 const users = [
     { edad: 13, nombre: "juan", apellido: "perez" },
     { edad: 19, nombre: "Pedro", apellido: "penrandes" },
@@ -5,10 +7,7 @@ const users = [
     { edad: 1, nombre: "juanita", apellido: "notancuma" },
 ];
 
-//primero compose recibira las funciones y los transformara en un arreglo.
-//luego recibira x que sera el input principal (asumo que users, en este caso)
-//Luego reduceRigth ejecutara 
-const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
+const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
 
 const filter = f => xs => xs.filter(f);
 const head = xs => xs[0];
@@ -18,6 +17,11 @@ const creaI = el => ({
 });
 const format = ele => `${ele.fullName} tiene ${ele.edad} años`;
 
-const composePrimerInfante = data => compose(format, creaI, head, filter(x => x.edad < 2))(data);
+const composePrimerInfante = data => pipe(
+    filter(x => x.edad < 2),
+    head, 
+    creaI, 
+    format
+    )(data);
 
 console.log(composePrimerInfante(users));
