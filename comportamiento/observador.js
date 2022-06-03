82b63e2a25280fd7c6e550183351c492f9275ee6
Leonardo <castroleonardo@live.cl>
2022-06-02 21:11:53 -0400

    Agrega carpeta de patronas de comportamiento

---
+++ comportamiento/observador.js
@@ -0,0 +1,24 @@
+//Usa listeners de distinto tipo para hacer observables a los cuales un objeto podra reaccionar
+//esto es casi pseudocodigo
+
+const user = new User();
+const init = () => {
+    user.on('event', funcionForEvent);
+};
+
+const funcionForEvent() {
+    //logica
+};
+
+app.init();
+
+
+//Por otro lado en la app o codigo algo debe despachar dicho evento
+const login = () => {
+    //logica de iniciar sesion por ejemplo
+
+    //se despacha el evento
+    user.trigger('event');
+}
+
+login();
