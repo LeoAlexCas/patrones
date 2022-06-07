afab7c0a76782c86a736a9b0cb415bfbb34389d5
Leonardo <castroleonardo@live.cl>
2022-06-06 20:55:29 -0400

    explicacion

--- comportamiento/iterador.js
+++ comportamiento/iterador.js
@@ -8,6 +8,11 @@ function* iterador(x = []) {
     }
 }
 
+//Cuando el metodo next() del iterador es llamado , el cuerpo de la función generadora es ejecutado hasta la primera expresión yield, 
+//la cual especifica el valor que será retornado por el iterador o con, yield*, delega a otra función generadora. El método next() retorna un objeto 
+//con una propiedad value que contiene el valor bajo el operador yield y una propiedad done que indica, con un booleano, si la función generadora ha 
+//hecho yield al último valor.
+
 const lista = [1, 2, 3, 4, 5, 6];
 const gen = iterador(lista);
 console.log(gen.next());
